20a427a4ac2dea8f10e203778b6156ae92e3c32f
NOXZPLAYZ <workfornoxzplayz@gmail.com>
2026-05-02 15:34:51 +0530

    feat: serve /ads and update ad banner image to ad1.png

--- server.js
+++ server.js
@@ -375,6 +375,7 @@ app.use(express.static(path.join(__dirname, 'public/html')));
 
 // Specifically serve models directory (Crucial for Vercel/Face-API)
 app.use('/models', express.static(path.join(__dirname, 'models')));
+app.use('/ads', express.static(path.join(__dirname, 'ads')));
 
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
